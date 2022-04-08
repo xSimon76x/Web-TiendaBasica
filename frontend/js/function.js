4c65d680a69f43f9e85992badefcea312e7c3d5c
SimonIvan <73963276+xSimon76x@users.noreply.github.com>
2022-04-08 14:36:55 -0400

    integrar datos por rutas del back, al front, mediante javscript vanilla. Se integraron los productos como las categorias, con un diseño similar a el al mostrado en el documento pdf

--- frontend/js/function.js
+++ frontend/js/function.js
@@ -1,10 +1,44 @@
-const lista = document.getElementById("listProduct");
+const listaProductos = document.getElementById("listProduct");
+const listaCategorias = document.getElementById("listCategorias");
 
-getData("/category").then((response) => {
-  //console.log(response.data.data[0].name);
+getData("/product").then((response) => {
   response.data.data.map((item) => {
     const categoryInList = document.createElement("div");
-    categoryInList.innerHTML = item.name;
-    lista.appendChild(categoryInList);
+    const rowSell = document.createElement("div");
+    categoryInList.className = "cardProducto";
+    categoryInList.innerHTML += `<img src="${item.url_image}">`;
+    categoryInList.innerHTML += `<p> ${item.name}<p/>`;
+
+    rowSell.className = "rowSell";
+    rowSell.innerHTML += `<p> $${item.price}<p/>`;
+    rowSell.innerHTML += `<i class="bi bi-cart-plus" style="font-size: 1rem; color: white"></i>`;
+    categoryInList.appendChild(rowSell);
+    listaProductos.appendChild(categoryInList);
   });
 });
+
+getData("/category").then((response) => {
+  response.data.data.map((item, index) => {
+    const categoryInList = document.createElement("div");
+    const nameCategoria = document.createElement("p");
+    nameCategoria.innerHTML += `${item.name}`;
+    // nameCategoria.id += `typeCategoria.${index}`;
+    // nameCategoria.setAttribute("onclick", searchByCategory(item.name));
+    categoryInList.className = "containerCategoria";
+    categoryInList.appendChild(nameCategoria);
+    listaCategorias.appendChild(categoryInList);
+  });
+});
+
+const cat = document.getElementById("typeCategoria.1");
+console.log(cat);
+
+const searchByCategory = (category) => {
+  getData("/product").then((response) => {
+    response.data.data.map((item) => {
+      {
+        item.typeCategory.name == category ? console.log(item.name) : false;
+      }
+    });
+  });
+};
